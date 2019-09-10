e2ce97c91daf38bdcdee950a8c0d2f34a274135e
Lukas Tönne <lutnne@microsoft.com>
2019-09-10 10:41:04 +0100

    Make grid/block index types equatable.

--- Utils/GridIndex.cs
+++ Utils/GridIndex.cs
@@ -1,6 +1,7 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License. See LICENSE in the project root for license information.
 
+using System;
 using Utils;
 
 namespace Shwarm.MathUtils
@@ -10,16 +11,13 @@ namespace Shwarm.MathUtils
         public const int BlockSize = 16;
         public const int BlockSize2 = 256;
         public const int BlockSize3 = 4096;
-        public const int BlockIndexMask = 0x0000000F;
-        public const int BlockIndexMask2 = 0x000000FF;
-        public const int BlockIndexMask3 = 0x00000FFF;
-        public const int BlockIndexShift = 4;
-        public const int BlockIndexShift2 = 8;
-        public const int BlockIndexShift3 = 12;
+        public const int BlockLog2Dim = 4;
+        public const int BlockLog2Dim2 = 8;
+        public const int BlockLog2Dim3 = 12;
 
         public static GridIndex BlockToGridIndex(BlockIndex blockIndex)
         {
-            return new GridIndex(blockIndex.i << BlockIndexShift, blockIndex.j << BlockIndexShift, blockIndex.k << BlockIndexShift);
+            return new GridIndex(blockIndex.i << BlockLog2Dim, blockIndex.j << BlockLog2Dim, blockIndex.k << BlockLog2Dim);
         }
         public static GridIndex BlockToGridIndex(BlockIndex blockIndex, GridIndex localIndex)
         {
@@ -32,36 +30,39 @@ namespace Shwarm.MathUtils
 
         public static BlockIndex GridToBlockIndex(GridIndex gridIndex)
         {
-            return new BlockIndex(gridIndex.i >> BlockIndexShift, gridIndex.j >> BlockIndexShift, gridIndex.k >> BlockIndexShift);
+            return new BlockIndex(gridIndex.i >> BlockLog2Dim, gridIndex.j >> BlockLog2Dim, gridIndex.k >> BlockLog2Dim);
         }
         public static BlockIndex GridToBlockIndex(GridIndex gridIndex, out GridIndex localIndex)
         {
-            BlockIndex blockIndex = new BlockIndex(gridIndex.i >> BlockIndexShift, gridIndex.j >> BlockIndexShift, gridIndex.k >> BlockIndexShift);
-            localIndex = new GridIndex(gridIndex.i & BlockIndexMask, gridIndex.j & BlockIndexMask, gridIndex.k & BlockIndexMask);
+            int blockMask = (1 << BlockLog2Dim) - 1;
+            BlockIndex blockIndex = new BlockIndex(gridIndex.i >> BlockLog2Dim, gridIndex.j >> BlockLog2Dim, gridIndex.k >> BlockLog2Dim);
+            localIndex = new GridIndex(gridIndex.i & blockMask, gridIndex.j & blockMask, gridIndex.k & blockMask);
             return blockIndex;
         }
         public static BlockIndex GridToBlockIndex(GridIndex gridIndex, out int cellIndex)
         {
-            BlockIndex blockIndex = new BlockIndex(gridIndex.i >> BlockIndexShift, gridIndex.j >> BlockIndexShift, gridIndex.k >> BlockIndexShift);
-            GridIndex localIndex = new GridIndex(gridIndex.i & BlockIndexMask, gridIndex.j & BlockIndexMask, gridIndex.k & BlockIndexMask);
+            int blockMask = (1 << BlockLog2Dim) - 1;
+            BlockIndex blockIndex = new BlockIndex(gridIndex.i >> BlockLog2Dim, gridIndex.j >> BlockLog2Dim, gridIndex.k >> BlockLog2Dim);
+            GridIndex localIndex = new GridIndex(gridIndex.i & blockMask, gridIndex.j & blockMask, gridIndex.k & blockMask);
             cellIndex = LocalToCellIndex(localIndex);
             return blockIndex;
         }
 
         public static GridIndex CellToLocalIndex(int cellIndex)
         {
+            int blockMask = (1 << BlockLog2Dim) - 1;
             return new GridIndex(
-                 cellIndex                      & BlockIndexMask,
-                (cellIndex >> BlockIndexShift)  & BlockIndexMask,
-                (cellIndex >> BlockIndexShift2) & BlockIndexMask);
+                 cellIndex                   & blockMask,
+                (cellIndex >> BlockLog2Dim)  & blockMask,
+                (cellIndex >> BlockLog2Dim2) & blockMask);
         }
         public static int LocalToCellIndex(GridIndex gridIndex)
         {
-            return gridIndex.i + (gridIndex.j << BlockIndexShift) + (gridIndex.k << BlockIndexShift2);
+            return gridIndex.i + (gridIndex.j << BlockLog2Dim) + (gridIndex.k << BlockLog2Dim2);
         }
     }
 
-    public struct GridIndex
+    public struct GridIndex : IEquatable<GridIndex>
     {
         public readonly int i;
         public readonly int j;
@@ -93,9 +94,14 @@ namespace Shwarm.MathUtils
         {
             return new GridIndex(a.i - b.i, a.j - b.j, a.k - b.k);
         }
+
+        public bool Equals(GridIndex other)
+        {
+            return this.i == other.i && this.j == other.j && this.k == other.k;
+        }
     }
 
-    public struct BlockIndex
+    public struct BlockIndex : IEquatable<BlockIndex>
     {
         public readonly int i;
         public readonly int j;
@@ -116,6 +122,11 @@ namespace Shwarm.MathUtils
         public override string ToString()
         {
             return "BlockIndex(" + i + ", " + j + ", " + k + ")";
+        }
+
+        public bool Equals(BlockIndex other)
+        {
+            return this.i == other.i && this.j == other.j && this.k == other.k;
         }
     }
 }
